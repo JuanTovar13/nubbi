fdf5c7a9ae991819d07a3546d9115129a22242d1
Juan Tovar <juanstovarm@gmail.com>
2026-04-07 22:50:48 -0500

    initial commit nubi

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,63 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { UserProvider, useUser } from "./providers/UserProvider";
-import { AxiosProvider } from "./providers/AxiosProvider";
-import { ToastProvider } from "./providers/ToastProvider";
-import { RoomsProvider } from "./providers/RoomsProvider";
-import { ChatProvider } from "./providers/ChatProvider";
-import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
-import { LoginPage } from "./pages/LoginPage";
-import { RegisterPage } from "./pages/RegisterPage";
-import { RoomsPage } from "./pages/RoomsPage";
-import { ChatPage } from "./pages/ChatPage";
 
-const AppRoutes = () => {
-  const { auth } = useUser();
 
-  return (
-    <Routes>
-      <Route
-        path="/login"
-        element={auth ? <Navigate to="/rooms" /> : <LoginPage />}
-      />
-      <Route
-        path="/register"
-        element={auth ? <Navigate to="/rooms" /> : <RegisterPage />}
-      />
-      <Route
-        path="/rooms"
-        element={
-          <PrivateRoute isAuth={Boolean(auth)} redirectTo="/login">
-            <RoomsProvider>
-              <RoomsPage />
-            </RoomsProvider>
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/rooms/:id"
-        element={
-          <PrivateRoute isAuth={Boolean(auth)} redirectTo="/login">
-            <ChatProvider>
-              <ChatPage />
-            </ChatProvider>
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to={auth ? "/rooms" : "/login"} />} />
-    </Routes>
-  );
-};
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <AxiosProvider>
-        <ToastProvider>
-          <UserProvider>
-            <AppRoutes />
-          </UserProvider>
-        </ToastProvider>
-      </AxiosProvider>
+  
+      
+    
     </BrowserRouter>
   );
 };
--- server/src/features/messages/message.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createMessageController, getMessagesController } from './message.controller';
-import { authMiddleware } from '../../middlewares/authMiddleware';
-
-export const router = Router({ mergeParams: true });
-
-router.use(authMiddleware);
-router.get('/', getMessagesController);
-router.post('/', createMessageController);
